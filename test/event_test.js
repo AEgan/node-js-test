843e2b6e8af3342fe4caa8a418af25bd97009a1a
Alex Egan <aegan@andrew.cmu.edu>
2014-02-21 00:16:43 -0500

    fixes test file and adds a test

--- test/event_test.js
+++ test/event_test.js
@@ -6,13 +6,43 @@ var Category = mongoose.model('Category');
 
 
 after(function(done) {
-	test_dbconnection.db.dropDatabase(function(){
+	test_db.connection.db.dropDatabase(function(){
 		test_db.connection.close(function(){
 	  	done();
 		});
 	});
 });
 
-describe('Event', function(){
-	console.log('here');
+describe('Category', function(){
+	beforeEach(function(done) {
+		Category.remove(done);
+
+		testCat = {
+			name: "Game",
+			active: true
+		};
+
+
+	});
+
+	after(function(done) {
+		Category.remove(done);
+	});
+
+	describe('#save()', function(){
+		var category_object;
+		beforeEach(function(done) {
+			category_object = new Category(testCat);
+			category_object.save(done);
+		});
+
+		it('test1: should have properties', function(done) {
+			category_object.save(function(err, returned) {
+				should.not.exist(err);
+				returned.should.have.property('name', 'Game');
+				returned.should.have.property('active', true);
+				done();
+			});
+		});
+	});
 });
